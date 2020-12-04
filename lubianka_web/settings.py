e7e6189352b73369681922db347c0801d1a39b9c
MortarMagnet <74789640+MortarMagnet@users.noreply.github.com>
2020-12-04 18:48:13 +0100

    It Works Now
    login: admin
    pass: Hazard45

--- lubianka_web/settings.py
+++ lubianka_web/settings.py
@@ -79,14 +79,26 @@ WSGI_APPLICATION = 'lubianka_web.wsgi.application'
 DATABASES = {
   'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lubianka-serv',
-        'USER': 'lubianka-serv',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
+        'NAME': 'eqokcett',
+        'USER': 'eqokcett',
+        'PASSWORD': 'zjdOMkX1nPmuNCYxOZGAMbqtzKmIGj3W',
+        'HOST': 'kandula.db.elephantsql.com',
         'PORT': '5432',
     }
 }
 
+
+
+# DATABASES = {
+#   'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'lubianka-serv',
+#         'USER': 'lubianka-serv',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
